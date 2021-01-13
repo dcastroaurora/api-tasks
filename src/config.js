8afa8e055c7b32c1060f1e2e20cd0fbff88d22e0
Diego <dcastroaurora@gmail.com>
2021-01-13 14:08:00 -0500

    Change DB default

--- src/config.js
+++ src/config.js
@@ -3,7 +3,5 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongodbURL:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://dcastro:marvelvsc2@cluster0.8npch.mongodb.net/taskapi?retryWrites=true&w=majority",
+  mongodbURL: process.env.MONGODB_URI || "mongodb://localhost/taskdb",
 };
